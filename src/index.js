140f3a2739031de75bb9dbe47ced5367806bc09d
kvkens <kvkens@qq.com>
2018-01-28 01:58:22 +0800

    添加参数更换代理

--- src/index.js
+++ src/index.js
@@ -1,39 +1,35 @@
-/*
- * @Author: Kvkens
+/* uba-server
+ * @Author: Kvkens(yueming@yonyou.com)
  * @Date:   2017-5-15 00:00:00
  * @Last Modified by:   Kvkens
- * @Last Modified time: 2017-12-19 19:33:56
+ * @Last Modified time: 2018-01-27 23:47:06
  */
 
 var chalk = require("chalk");
 var path = require("path");
 var express = require("express");
-var proxy = require('express-http-proxy');
+// var proxy = require("express-http-proxy");
+var proxy = require("http-proxy-middleware");
 var webpackDevMiddleware = require("webpack-dev-middleware");
 var webpack = require("webpack");
-var webpackConfig = require("./webpack.base");
+var util = require("./util");
+var webpackConfig = util.getConfig().devConfig;
 var app = express();
 var router = express.Router();
 var portfinder = require("portfinder");
-var fallback = require('connect-history-api-fallback');
+var history = require("connect-history-api-fallback");
 var compiler = webpack(webpackConfig);
-var mockConfig,
-  svrConfig,
-  proxyConfig,
-  staticConfig;
-var ubaConfig = require(path.resolve(".", "uba.config.js"));
+var mockConfig, svrConfig, proxyConfig, staticConfig;
+var ubaConfig = util.getConfig();
 
-try {
-  //读取服务器配置
-  svrConfig = ubaConfig.svrConfig;
-  //读取代理配置
-  proxyConfig = ubaConfig.proxyConfig;
-  //读取静态资源配置
-  staticConfig = ubaConfig.staticConfig;
-} catch (e) {
-  console.log(chalk.red(e));
-  process.exit(0);
-} finally {}
+
+//读取服务器配置
+svrConfig = ubaConfig.svrConfig;
+//读取代理配置
+proxyConfig = ubaConfig.proxyConfig;
+//读取静态资源配置
+staticConfig = ubaConfig.staticConfig;
+
 
 try {
   mockConfig = require(path.resolve(".", "uba.mock.js"));
@@ -64,7 +60,9 @@ function server() {
   //设置指定静态资源目录
   app.use(express.static(path.resolve('.', staticConfig.folder)));
   //设置browserHistory时
-    app.use(fallback())
+  if (svrConfig.historyApiFallback) {
+    app.use(history());
+  }
   //加载webpack处理
   app.use(webpackDevMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath,
@@ -74,8 +72,8 @@ function server() {
     }
   }));
 
+
   //开始加载Mock
-  console.log(chalk.yellow("\n/******************** Start loading mock server ********************/\n"));
   for (let item in mockConfig) {
     for (let i = 0; i < mockConfig[item].length; i++) {
       for (let url in mockConfig[item][i]) {
@@ -91,21 +89,26 @@ function server() {
       }
     }
   }
-  console.log(chalk.yellow("\n/******************** Mock server loaded completed *****************/\n"));
   app.use(router);
 
   //开始加载代理
-  console.log(chalk.yellow("\n/******************** Start loading proxy server ********************/\n"));
   proxyConfig.forEach(function (element) {
     if (element.enable) {
-      app.use(element.router, proxy(element.url, element.options));
+      app.use(element.router, proxy({
+        target: element.url,
+        logLevel : "debug",
+        changeOrigin: true,
+        onProxyRes: (proxyRes) => {
+          proxyRes.headers["Uba-Server-Proxy"] = "true";
+        }
+      }));
+      // app.use(element.router, proxy(element.url, element.options));
       console.log(chalk.green(`[proxy] : ${element.router} to ${element.url}`));
     }
   });
-  console.log(chalk.yellow("\n/******************** Proxy server loaded completed *****************/\n"));
+
 
   app.use(require("webpack-hot-middleware")(compiler));
-
   //检测端口是否冲突设置
   portfinder.basePort = svrConfig.port;
   portfinder.getPort((err, port) => {
@@ -113,9 +116,13 @@ function server() {
       throw err;
     }
     app.listen(port, svrConfig.host, function () {
-      console.log(chalk.yellow("\n/******************** Start dev server *****************/\n"));
-      console.log(chalk.green(`[uba] : Listening on port http://${svrConfig.host}:${port}`));
-      console.log(chalk.yellow("\n/******************** O(∩_∩)O *****************/\n"));
+      console.log();
+      console.log(chalk.green(`********************************************`));
+      console.log(chalk.yellow(` ❤️  uba-dev-server`));
+      console.log(chalk.green(` [core] : v${util.getPkg().version}`));
+      console.log(chalk.green(` [http] : http://${svrConfig.host}:${port}`));
+      console.log(chalk.green(`********************************************`));
+      console.log();
     });
   });
 
---
+++ src/util.js
@@ -0,0 +1,31 @@
+/* util
+ * @Author: Kvkens(yueming@yonyou.com)
+ * @Date:   2017-5-15 00:00:00
+ * @Last Modified by:   Kvkens
+ * @Last Modified time: 2018-01-27 23:29:56
+ */
+
+var path = require("path");
+var chalk = require("chalk");
+var webpack = require("webpack");
+
+exports.getConfig = function () {
+    var ubaConfig;
+  try {
+    ubaConfig = require(path.resolve(".", "uba.config.js"));
+    //new webpack.ProgressPlugin()
+    ubaConfig.devConfig.plugins.push(new webpack.ProgressPlugin());
+  } catch (e) {
+    console.log(chalk.red(e));
+    console.log(chalk.yellow("Please check uba.config.js configuration file"));
+    process.exit(0);
+  }
+  return ubaConfig;
+}
+
+/**
+ * 获取package.json
+ */
+exports.getPkg = () => {
+  return require("../package.json");
+}
